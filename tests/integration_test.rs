6e29b881ae15e8782b5d550c3133150d6aaa3bfe
Radonirinaunimi <rrabeman@nikhef.nl>
2025-07-03 15:43:50 +0200

    Improve more the integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -7,18 +7,41 @@ fn test_xf_at_knots() {
     let pdf = load(pdf_set_path);
 
     // test for `xf` at x_0 and q_0
-    assert!((dbg!(pdf.knot_array.xf(0, 0, 21)) - 0.14844111).abs() < 1e-8);
-    assert!((dbg!(pdf.knot_array.xf(0, 0, 1)) - 1.4254154).abs() < 1e-8);
-    assert!((dbg!(pdf.knot_array.xf(0, 0, 2)) - 1.4257712).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(0, 0, 21) - 0.14844111).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(0, 0, 1) - 1.4254154).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(0, 0, 2) - 1.4257712).abs() < 1e-8);
+
+    // test for `xf` at x_1 and q_0
+    assert!((pdf.knot_array.xf(1, 0, 21) - 0.15395356).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(1, 0, 1) - 1.3883271).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(1, 0, 2) - 1.3887002).abs() < 1e-8);
+
+    // test for `xf` at x_1 and q_2
+    assert!((pdf.knot_array.xf(1, 2, 21) + 3.164867).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(1, 2, 1) - 1.9235433).abs() < 1e-8);
+    assert!((pdf.knot_array.xf(1, 2, 2) - 1.9239212).abs() < 1e-8);
 }
 
 #[test]
-fn test_xfxq2() {
+fn test_xfxq2_at_knots() {
     let pdf_set_path = Path::new("./_lhapdf/NNPDF40_nnlo_as_01180");
     let pdf = load(pdf_set_path);
 
-    // test for `xf` at x_0 and q_0
-    assert!((dbg!(pdf.xfxq2(21, 1e-9, 1.65 * 1.65)) - 0.14844111).abs() < 1e-8);
-    assert!((dbg!(pdf.xfxq2(1, 1e-9, 1.65 * 1.65)) - 1.4254154).abs() < 1e-8);
-    assert!((dbg!(pdf.xfxq2(2, 1e-9, 1.65 * 1.65)) - 1.4257712).abs() < 1e-8);
+    // test for x=1e-9 and Q=1.65
+    let q2 = 1.65 * 1.65;
+    assert!((pdf.xfxq2(21, 1e-9, q2) - 0.14844111).abs() < 1e-8);
+    assert!((pdf.xfxq2(1, 1e-9, q2) - 1.4254154).abs() < 1e-8);
+    assert!((pdf.xfxq2(2, 1e-9, q2) - 1.4257712).abs() < 1e-8);
+
+    // test for x=1.2970848e-9 and Q=1.65
+    let q2 = 1.65 * 1.65;
+    assert!((pdf.xfxq2(21, 1.2970848e-9, q2) - 0.15395356).abs() < 1e-8);
+    assert!((pdf.xfxq2(1, 1.2970848e-9, q2) - 1.3883271).abs() < 1e-8);
+    assert!((pdf.xfxq2(2, 1.2970848e-9, q2) - 1.3887002).abs() < 1e-8);
+
+    // test for x=1.2970848e-9 and Q=1.9429053
+    let q2 = 1.9429053 * 1.9429053;
+    assert!((pdf.xfxq2(21, 1.2970848e-9, q2) + 3.164867).abs() < 1e-8);
+    assert!((pdf.xfxq2(1, 1.2970848e-9, q2) - 1.9235433).abs() < 1e-8);
+    assert!((pdf.xfxq2(2, 1.2970848e-9, q2) - 1.9239212).abs() < 1e-8);
 }
