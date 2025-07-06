03d51a8be524fde311c25c495a9d52421231337e
Radonirinaunimi <rrabeman@nikhef.nl>
2025-07-07 00:28:34 +0200

    Instantiate the C-API and CLI libraries

---
+++ neopdf_cli/src/lib.rs
@@ -0,0 +1,18 @@
+//! Command Line Interface (CLI) for `neopdf`
+
+/// TODO
+#[must_use]
+pub const fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
