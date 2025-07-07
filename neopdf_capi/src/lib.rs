7bf650a55d56db9a540e03dd67cae39194351aa3
Radonirinaunimi <rrabeman@nikhef.nl>
2025-07-07 16:21:55 +0200

    Implement the C-API

--- neopdf_capi/src/lib.rs
+++ neopdf_capi/src/lib.rs
@@ -1,18 +1,139 @@
-//! C-API for `neopdf`
+//! The C-language interface for `NeoPDF`
 
+use neopdf::pdf::PDF;
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+/// Opaque pointer to a PDF object.
+pub struct NeoPDF(PDF);
+
+/// Loads a given member of the PDF set.
+///
+/// # Panics
+///
 /// TODO
-#[must_use]
-pub const fn add(left: u64, right: u64) -> u64 {
-    left + right
+///
+/// # Safety
+///
+/// The `pdf_name` C string must be null-terminated and valid UTF-8.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_load(pdf_name: *const c_char, member: usize) -> *mut NeoPDF {
+    let c_str = unsafe { CStr::from_ptr(pdf_name) };
+    let pdf_name = c_str.to_str().expect("Invalid UTF-8 string");
+    let pdf = PDF::load(pdf_name, member);
+    Box::into_raw(Box::new(NeoPDF(pdf)))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Frees a PDF object.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `NeoPDF` object previously
+/// allocated by `neopdf_pdf_load`.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_free(pdf: *mut NeoPDF) {
+    if pdf.is_null() {
+        return;
     }
+    unsafe { drop(Box::from_raw(pdf)) };
+}
+
+/// Retrieves the `x_min` for this PDF set.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_x_min(pdf: *mut NeoPDF) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.x_min()
+}
+
+/// Retrieves the `x_max` for this PDF set.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_x_max(pdf: *mut NeoPDF) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.x_max()
+}
+
+/// Retrieves the `q2_min` for this PDF set.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_q2_min(pdf: *mut NeoPDF) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.q2_min()
+}
+
+/// Retrieves the `q2_max` for this PDF set.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_q2_max(pdf: *mut NeoPDF) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.q2_max()
+}
+
+/// Interpolates the PDF value (xf) for a given flavor, x, and Q2.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_xfxq2(pdf: *mut NeoPDF, id: i32, x: f64, q2: f64) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.xfxq2(id, x, q2)
+}
+
+/// Computes the `alpha_s` value at a given Q2.
+///
+/// # Panics
+///
+/// TODO
+///
+/// # Safety
+///
+/// The `pdf` pointer must be a valid pointer to a `CPDF` object.
+#[no_mangle]
+pub unsafe extern "C" fn neopdf_pdf_alphas_q2(pdf: *mut NeoPDF, q2: f64) -> f64 {
+    assert!(!pdf.is_null());
+    let pdf_obj = unsafe { &(*pdf).0 };
+    pdf_obj.alphas_q2(q2)
 }
---
+++ neopdf_capi/tests/check-capi.cpp
@@ -0,0 +1,63 @@
+#include <neopdf_capi.h>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <tuple>
+#include <vector>
+
+const double TOLERANCE= 1e-16;
+
+int main() {
+    NeoPDF* pdf = neopdf_pdf_load("NNPDF40_nnlo_as_01180", 0);
+
+    std::vector<std::tuple<int, double, double, double>> cases = {
+        {1, 1e-9, 1.65 * 1.65, 1.4254154},
+        {2, 1e-9, 1.65 * 1.65, 1.4257712},
+        {21, 1e-9, 1.65 * 1.65, 0.14844111},
+        {1, 1.2970848e-9, 1.65 * 1.65, 1.3883271},
+        {2, 1.2970848e-9, 1.65 * 1.65, 1.3887002},
+        {21, 1.2970848e-9, 1.65 * 1.65, 0.15395356},
+        {1, 1.2970848e-9, 1.9429053 * 1.9429053, 1.9235433},
+        {2, 1.2970848e-9, 1.9429053 * 1.9429053, 1.9239212},
+        {21, 1.2970848e-9, 1.9429053 * 1.9429053, -3.164867}
+    };
+
+    // Headers of the table to print the results
+    std::cout << std::left
+        << std::setw(6) << "pid"
+        << std::setw(15) << "x"
+        << std::setw(15) << "Q2"
+        << std::setw(15) << "LHAPDF"
+        << std::setw(15) << "NeoPDF"
+        << std::setw(15) << "Rel. Diff." << "\n";
+    std::cout << std::string(71, '-') << "\n";
+
+    for (size_t i = 0; i < cases.size(); ++i) {
+        int pid;
+        double x, q2, expected;
+        std::tie(pid, x, q2, expected) = cases[i];
+        double result = neopdf_pdf_xfxq2(pdf, pid, x, q2);
+        double reldif = std::abs(result - expected) / expected;
+
+        assert(std::abs(result - expected) < TOLERANCE);
+
+        // Print the results as a table
+        std::cout << std::scientific << std::setprecision(8)
+            << std::left
+            << std::setw(6)  << pid
+            << std::setw(15) << x
+            << std::setw(15) << q2
+            << std::right
+            << std::setw(15) << expected
+            << std::setw(15) << result
+            << std::setw(15) << reldif << "\n";
+
+    }
+
+    // Delete PDF object from memory
+    neopdf_pdf_free(pdf);
+
+    return EXIT_SUCCESS;
+}
